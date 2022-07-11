64e2faa2344e8b3d0e7ccdc42e6eb98869f552ef
hiralius <nyanko9@gmail.com>
2022-07-11 12:53:41 +0900

    艦隊の進撃先が取得可能となるように対応 各艦の戦闘直後のHP反映を連合艦隊出撃時にも対応 単艦退避の反映に対応 MasterDataViewerプラグインにて進撃位置等の表示に対応(不具合あり)

--- source/Plugins/MasterDataViewer/ViewModels/PortalViewModel.cs
+++ source/Plugins/MasterDataViewer/ViewModels/PortalViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -14,65 +15,189 @@ namespace Grabacr07.KanColleViewer.Plugins.ViewModels
 {
 	public class MasterData : NotificationObject
 	{
-		private string _text;
+		private string _info;
 
-		public string Text
+		public string Info
 		{
-			get { return _text; }
+			get { return _info; }
 			set
 			{
-				if (_text == value) return;
-				_text = value;
+				if (_info == value) return;
+				_info = value;
 				RaisePropertyChanged();
 			}
 		}
+
+		/// <summary>
+		/// 海域名
+		/// </summary>
+		private string _area;
+		public string Area
+		{
+			get { return _area; }
+			set
+			{
+				if (_area == value) return;
+				_area = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		/// <summary>
+		/// 海域番号　
+		/// </summary>
+		private string _mapid;
+		public string MapID
+		{
+			get { return _mapid; }
+			set
+			{
+				if (_mapid == value) return;
+				_mapid = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		/// <summary>
+		/// 進撃位置
+		/// </summary>
+		private string _cellid;
+		public string CellID
+		{
+			get { return _cellid; }
+			set 
+			{
+				if (_cellid == value) return;
+				_cellid = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		public bool OverAlphabet { get; set; }
+
+		public string[] enemies;
+
+		public MasterData()
+		{
+			enemies = new string[20];
+		}
+
 	}
 
 
 	public class PortalViewModel : ViewModel
 	{
-		private MasterData _Master;
+		//public event EventHandler<NotifyEventArgs> NotifyRequested;
 
-		public event EventHandler<NotifyEventArgs> NotifyRequested;
-
-		public MasterData Master { get { return _Master; } }
+		public MasterData Master { get; }
 
 		public PortalViewModel(KanColleProxy proxy)
 		{
-			_Master = new MasterData();
+			Master = new MasterData();
 
-			proxy.api_req_sortie_battleresult
+			_ = proxy.api_req_map_start
+				.TryParse<kcsapi_map_start>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(data =>
+				{
+					var result = data.Data;
+
+					var area = KanColleClient.Current.Master.MapAreas[result.api_maparea_id];
+					var map = KanColleClient.Current.Master.MapInfos[(result.api_maparea_id * 10) + result.api_mapinfo_no];
+					Master.Area = $"{area.Name} {map.Name}";
+					Master.MapID = $"{result.api_maparea_id}-{result.api_mapinfo_no}";
+					Master.CellID = $"{result.api_no}";
+
+					Master.OverAlphabet = (result.api_cell_data.Length > 25);
+					ChangeCellID(result.api_no);
+				});
+			_ = proxy.api_req_map_next
+				.TryParse<kcsapi_map_next>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(data =>
+				{
+					ChangeCellID(data.Data.api_no);
+				});
+
+
+			_ = proxy.api_req_sortie_battleresult
 				.TryParse<kcsapi_battleresult>()
 				.Where(x => x.IsSuccess)
-				.Subscribe(data => BattleResult(data));
+				.Subscribe(data => BattleResult(data.Request));
+
+			_ = proxy.api_req_combined_battle_battleresult
+				.TryParse<kcsapi_combined_battle_battleresult>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(data =>
+				{
+					BattleResult(data.Request);
+				});
 		}
 
-		private void BattleResult(SvData<kcsapi_battleresult> data)
+		private void ChangeCellID(int id)
+		{
+			if (Master.OverAlphabet)
+			{
+				Master.CellID = $"{id}";
+			}
+			else
+			{
+				Master.CellID = $"\'{(char)('@' + id)}\'";
+			}
+		}
+		private void BattleResult(NameValueCollection request)
 		{
 			string result = "";
 
-			if (KanColleClient.Current.Homeport.Organization.Combined) return;
+			//foreach (var key in request.AllKeys)
+			//{
+			//	result += $"{key} = {request[key]}\n";
+			//}
+			//result += "\n";
 
 			// 出撃艦隊を取り出す
-			var fleet = KanColleClient.Current.Homeport.Organization.Fleets.First(x => x.Value.IsInSortie).Value;
-
-			result += $"出撃海域: {data.Data.api_quest_name}\n";
+			var fleets = KanColleClient.Current.Homeport.Organization.Fleets.Where(x => x.Value.IsInSortie).Select(x => x.Value).ToArray();
 
 			// 戦闘後HPを表示
+			if (fleets.Length > 1)
+			{
+				result = "主力艦隊\n";
+			}
+			else
+			{
+				result = $"{fleets[0].Name}\n";
+			}
+
 			for (int i = 0; i < 10; i++)
 			{
-				string hp = data.Request[$"api_l_value[{i}]"];
+				string hp = request[$"api_l_value[{i}]"];
 				if (hp == null)
 				{
 					continue;
 				}
-				result += $"{fleet.Ships[i].Info.Name} = {hp}\n";
+				result += $"{fleets[0].Ships[i].Info.Name} = {hp}\n";
 			}
 			result += "\n";
 
+			if (fleets.Length > 1)
+			{
+				result += "随伴艦隊\n";
+				for (int i = 0; i < 10; i++)
+				{
+					string hp = request[$"api_l_value2[{i}]"];
+					if (hp == null)
+					{
+						continue;
+					}
+					result += $"{fleets[1].Ships[i].Info.Name} = {hp}\n";
+				}
+				result += "\n";
+			}
+
+			result += "敵艦隊\n";
 			for (int i = 0; i < 10; i++)
 			{
-				string hp = data.Request[$"api_l_value3[{i}]"];
+				string hp = request[$"api_l_value3[{i}]"];
 				if (hp == null)
 				{
 					continue;
@@ -80,7 +205,8 @@ namespace Grabacr07.KanColleViewer.Plugins.ViewModels
 
 				result += $"enemy{i + 1} = {hp}\n";
 			}
-			Master.Text = result;
+			
+			Master.Info = result;
 		}
 
 
