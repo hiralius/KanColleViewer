b9e909f3461d9925e8e5b9ce20a557196e927b73
hirahara <m.hirahara@cosmo.co.jp>
2022-09-15 18:23:41 +0900

    デバッグビルド時のDebugタブにデバッグログ表示を追加

--- source/Grabacr07.KanColleViewer/ViewModels/Dev/DebugTabViewModel.cs
+++ source/Grabacr07.KanColleViewer/ViewModels/Dev/DebugTabViewModel.cs
@@ -1,17 +1,56 @@
-﻿using System;
+using Livet;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Grabacr07.KanColleViewer.Composition;
 
 namespace Grabacr07.KanColleViewer.ViewModels.Dev
 {
+	public class Tracer : TraceListener
+	{
+		private Action<string> writeMsg;
+
+		public Tracer(Action<string> action)
+		{
+			writeMsg = action;
+		}
+
+		public override void Write(string message)
+		{
+			writeMsg(message);
+		}
+
+		public override void WriteLine(string message)
+		{
+			Write(message + "\n");
+		}
+	}
+
 	public class DebugTabViewModel : TabItemViewModel
 	{
+		private string _msg;
+		public string TraceMessage { get => _msg; set => RaisePropertyChangedIfSet(ref _msg, value); }
+
+		private readonly Tracer trace;
+
+
 		public override string Name
 		{
 			get { return Properties.Resources.Debug; }
 			protected set { throw new NotImplementedException(); }
+		}
+
+		public DebugTabViewModel()
+		{
+			trace = new Tracer(msg => TraceMessage += msg);
+			_ = Debug.Listeners.Add(trace);
+		}
+
+		public void Clear()
+		{
+			TraceMessage = "";
 		}
 	}
 }
